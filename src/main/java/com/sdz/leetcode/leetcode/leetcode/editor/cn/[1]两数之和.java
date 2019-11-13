47a32e73f8aab462b54242a25138202d2d6729f3
DeZhi Song <songdezhi@qiyi.com>
2019-11-13 14:20:03 +0800

    新题解

--- src/main/java/com/sdz/leetcode/leetcode/leetcode/editor/cn/[1]两数之和.java
+++ src/main/java/com/sdz/leetcode/leetcode/leetcode/editor/cn/[1]两数之和.java
@@ -14,9 +14,9 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+/*class Solution {
     public int[] twoSum(int[] nums, int target) {
         return null;
     }
-}
+}*/
 //leetcode submit region end(Prohibit modification and deletion)
